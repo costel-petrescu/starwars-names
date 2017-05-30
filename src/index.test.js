5e4773174f7530129e4c474589e603c93f0afd62
Costel Petrescu <costel.petrescu12@gmail.com>
2017-05-30 12:35:20 +0300

    replace starWarNames with starWarsNames

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,7 @@
 var starWar = require('./index');
 var expect = require('chai').expect;
 
-diescribe('starwars-names', function() {
+describe('starwars-names', function() {
     it('should work!', function(){
         expect(true).to.be.true;
     });
