4b09cbe5fe8ad907039438c29dae90ac053048ed
Costel Petrescu <costel.petrescu12@gmail.com>
2017-05-30 09:42:08 +0300

    rename

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# starwars-names
+# starwar-names
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 var uniqueRandomArray = require('unique-random-array');
-var starWarsNames = require('./starwars-names.json');
+var starWarNames = require('./starwars-names.json');
 
 module.exports = {
-    all: starWarsNames,
-    random: uniqueRandomArray(starWarsNames)
+    all: starWarNames,
+    random: uniqueRandomArray(starWarNames)
 };
