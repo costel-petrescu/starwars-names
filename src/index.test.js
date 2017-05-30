9c6823b837843017a1a74583a94c1d05877307f9
Costel Petrescu <costel.petrescu12@gmail.com>
2017-05-30 12:30:10 +0300

    rename

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var starWar = require('./index');
+var expect = require('chai').expect;
+
+diescribe('starwars-names', function() {
+    it('should work!', function(){
+        expect(true).to.be.true;
+    });
+});
